474b7d2e0a3c882443eab24e54fa60d9aca0038f
Lagdot <lagdot@users.noreply.github.com>
2025-08-18 18:40:42 +0200

    fixed wrong configurations I accidentalnfigurations I accidentally ported over from my other build

--- config.def.h
+++ config.def.h
@@ -1,13 +1,13 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 5;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx	    = 10;	/* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=20" };
-static const char dmenufont[]       = "monospace:size=20";
+static const char *fonts[]          = { "monospace:size=9" };
+static const char dmenufont[]       = "monospace:size=9";
 //background color
 static const char col_gray1[]       = "#222222";
 //inactive window border color
@@ -15,9 +15,9 @@ static const char col_gray2[]       = "#444444";
 //font color
 static const char col_gray3[]       = "#bbbbbb";
 //current tag and current window font color
-static const char col_gray4[]       = "#bbbbbb";
+static const char col_gray4[]       = "#444444";
 //top bar secondary color and active window border color
-static const char col_cyan[]        = "#385c66";
+static const char col_cyan[]        = "#d79921";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
