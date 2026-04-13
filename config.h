18be3c406914228edb56597d9b93d3fd67431563
Ladot <lagdot@users.noreply.github.com>
2026-04-13 21:26:09 +0200

    finally changed firefox to surf in the keybinds

--- config.h
+++ config.h
@@ -3,7 +3,6 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const Gap default_gap	    = {.isgap = 1, .realgap = 10, .gappx = 10};
-/*static const unsigned int gappx	    = 10;	*//* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -75,7 +74,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "xterm", NULL };
-static const char *firefoxcmd[] = { "firefox", NULL };
+static const char *firefoxcmd[] = { "surf, NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -85,9 +84,6 @@ static const Key keys[] = {
 	{ MODKEY,			XK_Return, spawn,      	   SHCMD("st") },
 	{ MODKEY|ShiftMask,		XK_l, 	   spawn,	   SHCMD("xlockmenu") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
-//	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-//	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-//gappx	    = 10;
 	{ MODKEY,                       XK_z,      setgaps,	   {.i = 10 } },
 	STACKKEYS(MODKEY,                          focus)
 	STACKKEYS(MODKEY|ShiftMask,                push)
